f9fc99b9e471d8d465e743a9e095a2539bbf5ab6
Alexander Pershin <info@devlab.pro>
2024-12-17 12:36:35 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
-nginx &
-python3 web.py
+#!/usr/bin/env bashio
+
+set -o allexport
+  ZB_ESP_DEVICE_PORT="$(bashio::config 'ZB_ESP_DEVICE_PORT')"
+set +o allexport
+
+exec nginx & python3 web.py $ZB_ESP_DEVICE_PORT
